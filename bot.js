842570553a831c99e6043ecacdbcc5371cffc230
Kacpersliwinski <sliwinskikacper@wp.pl>
2018-07-04 19:15:39 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,12 @@
+const commando = require('discord.js-commando');
+const bot = new commando.CommandoClient({
+    commandPrefix: '!',
+    owner: "191146743163781120"
+});
+bot.registry.registerDefaults();
+bot.registry.registerGroup('functional', 'Functional');
+bot.registry.registerCommandsIn(__dirname + "/commands/");
+
+bot.login("NDY0MTI3NzQwNDg4OTc0MzYw.Dh6cQw.IhVnz6jzXD9iso8nhceg9dNyuBw");
+
+client.login(process.env.NDY0MTI3NzQwNDg4OTc0MzYw.Dh6cQw.IhVnz6jzXD9iso8nhceg9dNyuBw);
